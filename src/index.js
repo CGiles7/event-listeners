31075f528c76e8b13a470eec8186dfffdd0a3429
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-14 23:45:52 -0400

    add event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -4,14 +4,53 @@ import "./styles.css";
 Add event listeners to the `.expand_button` buttons
 */
 function expandArticleBody() {
-  // your code here
+  const allButtons = document.querySelectorAll(".expand_button")
+  allButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      if(button.innerText === "V") {
+        const articleControl = button.parentElement;
+        const articleHead = articleControl.parentElement;
+        const article = articleHead.parentElement;
+        const articleBody = article.querySelector(".article_body");
+        articleBody.style.display = "none"
+        button.innerText = ">";
+      } else {
+        const articleControl = button.parentElement;
+        const articleHead = articleControl.parentElement;
+        const article = articleHead.parentElement;
+        const articleBody = article.querySelector(".article_body");
+        articleBody.style.display = "block"
+        button.innerText = "V";
+      }
+    })
+  })
 }
+
 
 /*
 Add event listeners to the `.highlightBtn` buttons
 */
 function highlightArticle() {
-  // your code here
+  const allButtons = document.querySelectorAll(".highlightBtn")
+  allButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      if(button.innerText === "+") {
+        const articleControl = button.parentElement;
+        const articleHead = articleControl.parentElement;
+        const article = articleHead.parentElement;
+        article.classList.add("highlight")
+        button.innerText = "-";
+      } else {
+        const articleControl = button.parentElement;
+        const articleHead = articleControl.parentElement;
+        const article = articleHead.parentElement;
+        article.classList.remove("highlight")
+        button.innerText= "+";
+      }
+    })
+  })
 }
 
 function main() {
